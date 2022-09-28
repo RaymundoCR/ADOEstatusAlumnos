c4bda978c79e357320f9ad143d6b69b93fb18298
RaymundoCR <111365842+RaymundoCR@users.noreply.github.com>
2022-09-28 18:54:52 -0500

    Res1

---
+++ ADOEstatusAlumnos/CRUDEstatus.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ADOEstatusAlumnos
+{
+    internal class CRUDEstatus : ICRUD
+    {
+        public static string _cnnString = ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString;
+        public static List<Estatus> _listaStatusAl = new List<Estatus>();
+        public static String _query;
+        public static SqlCommand _comando;
+        public List<Estatus> Consultar()
+        {
+            _listaStatusAl = new List<Estatus>();
+            _query = $"select * from EstatusAlumnos";
+            using (SqlConnection con = new SqlConnection(_cnnString))
+            {
+                _comando = new SqlCommand(_query, con);
+                _comando.CommandType = CommandType.Text;
+                con.Open();
+                SqlDataReader reader = _comando.ExecuteReader();
+                while (reader.Read())
+                {
+                    _listaStatusAl.Add(
+                    new Estatus()
+                    {
+                        id = Convert.ToInt32(reader["id"]),
+                        Clave = reader["Clave"].ToString(),
+                        Nombre = reader["Nombre"].ToString()
+                    }
+                );
+                }
+                con.Close();
+            }
+            return _listaStatusAl;
+        }
+        static Estatus _objEstatus = new Estatus();
+        public Estatus Consultar(int id)
+        {
+            try
+            {
+                _listaStatusAl = new List<Estatus>();
+                _query = $"select * from EstatusAlumnos where id = {id}";
+                using (SqlConnection con = new SqlConnection(_cnnString))
+                {
+                    _comando = new SqlCommand(_query, con);
+                    _comando.CommandType = CommandType.Text;
+                    con.Open();
+                    SqlDataReader reader = _comando.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        _objEstatus = new Estatus()
+                        {
+                            id = Convert.ToInt32(reader["id"]),
+                            Clave = reader["Clave"].ToString(),
+                            Nombre = reader["Nombre"].ToString()
+                        };
+                    }
+                    con.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return _objEstatus;
+
+        }
+
+        public int Agregar(Estatus status)
+        {
+            int idEstatus = 0;
+            try
+            {
+                _query = ($"agregarEstatusAlumno");
+                using (SqlConnection con = new SqlConnection(_cnnString))
+                {
+                    _comando = new SqlCommand(_query, con);
+                    _comando.CommandType = CommandType.StoredProcedure;
+                    _comando.Parameters.AddWithValue("@Clave", status.Clave);
+                    _comando.Parameters.AddWithValue("@Nombre", status.Nombre);
+                    con.Open();
+                    idEstatus = (Int32)_comando.ExecuteScalar();
+                    con.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return idEstatus;
+        }
+
+        public void Actualizar(Estatus status)
+        {
+            _query = $"update EstatusAlumnos set Clave = '{status.Clave}', Nombre = '{status.Nombre}' where id = {status.id}";
+            using (SqlConnection con = new SqlConnection(_cnnString))
+            {
+                _comando = new SqlCommand(_query, con);
+                _comando.CommandType = CommandType.Text;
+                con.Open();
+                _comando.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        public void Eliminar(Estatus status)
+        {
+            _query = $"delete EstatusAlumnos where id = {status.id}";
+            using (SqlConnection con = new SqlConnection(_cnnString))
+            {
+                _comando = new SqlCommand(_query, con);
+                _comando.CommandType = CommandType.Text;
+                con.Open();
+                _comando.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+}
---
+++ ADOEstatusAlumnos/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOEstatusAlumnos
+{
+    public class Program
+    {
+        static Estatus objEstatus = new Estatus();
+        public static List<Estatus> _listaStatusAl = new List<Estatus>();
+        static void Main(string[] args)
+        {
+            string opcion;
+            CRUDEstatus objCrud = new CRUDEstatus();
+            do
+            {
+                Console.WriteLine("SELECIONE UNA OPCION \n1.- Consultar Todos \n2.- Consultar Solo uno \n3.- Agregar  \n4.- Actualizar \n5.- Eliminar \n6.- Terminar");
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Usted selecciono la opcion 1 Consultar todos\n\n");
+                        _listaStatusAl = objCrud.Consultar();
+                        try
+                        {
+                            foreach (Estatus s in _listaStatusAl)
+                            {
+                                Console.Write($"{s.id} {s.Clave} {s.Nombre}");
+                                Console.WriteLine();
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Usted selecciono la opcion 2 Consultar uno\n\n");
+                        Console.WriteLine("Por favor ingrese el id status a consultar");
+                        int val = Convert.ToInt32(Console.ReadLine());
+                        objEstatus = objCrud.Consultar(val);
+                        Console.Write($"{objEstatus.id} {objEstatus.Clave} {objEstatus.Nombre}");
+                        Console.WriteLine();
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "3":
+                        objEstatus = new Estatus();
+                        Console.Clear();
+                        try
+                        {
+                            Console.WriteLine("Usted selecciono la opcion 3 Agregar un nuevo EstatusAlumnos\n\n");
+                            //Console.WriteLine("Por favor digite el id");
+                            //objEstatus.id = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Por favor digite la clave del status");
+                            objEstatus.Clave = Console.ReadLine();
+                            Console.WriteLine("Por favor digite el nombre del status");
+                            objEstatus.Nombre = Console.ReadLine();
+                            int res = objCrud.Agregar(objEstatus);
+                            Console.WriteLine($"Se ha agregado correctamente, agregado con el id: {res}");
+                            Console.ReadKey();
+                        }catch(Exception e) { Console.WriteLine(e.Message); }
+                        Console.Clear();
+                        break;
+                    case "4":
+                        Console.Clear();
+                        try
+                        {
+                            Console.WriteLine("Usted selecciono la opcion 4 Actualizar estatusAlumnos\n\n");
+                            Console.WriteLine("Por favor digite el id del estatus que se requiere actualizar");
+                            objEstatus.id = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Por favor digite la nueva clave del estatus");
+                            objEstatus.Clave = Console.ReadLine();
+                            Console.WriteLine("Por favor digite el nuevo nombre");
+                            objEstatus.Nombre = Console.ReadLine();
+                            objCrud.Actualizar(objEstatus);
+                            Console.WriteLine("Se ha actualizado correctamente");
+                            Console.ReadKey();
+                        }catch( Exception e) { Console.WriteLine(e.Message); }
+                        Console.Clear();
+                        break;
+                    case "5":
+                        Console.Clear();
+                        Console.WriteLine("Usted selecciono la opcion 5 Eliminar estatusAlumnos\n\n");
+                        Console.WriteLine("Por favor digite el id del estatus a eliminar");
+                        objEstatus.id = Convert.ToInt32(Console.ReadLine());
+                        objCrud.Eliminar(objEstatus);
+                        Console.Clear();
+                        break;
+                    case "6":
+                        Console.Clear();
+                        Console.WriteLine("Usted a salido de la áplicación\n\n");
+
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("La opcion seleccionada no existe\n\n");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (opcion != "6");
+
+        }
+    }
+}
